1b1a74ef3be454ffe3cddb6941f677ce73548f49
MehmetBardakci <m.bardakciw@gmail.com>
2025-07-29 21:22:49 +0300

    logo problem

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -10,7 +10,7 @@ const Navbar = () => {
     <nav className="pt-5 flex justify-between w-full items-center z-40">
       <HamburgerButton toggle={openMenu} setToggle={setOpenMenu} />
     <div className="w-[95.41px] h-[58.97px] relative">
-      <Image src={"/images/logo.svg"} alt="brandLogo" fill className="object-cover" />
+      <Image src={"/images/Logo.svg"} alt="brandLogo" fill className="object-cover" />
     </div>
     <span className="span-mont cursor-pointer relative max-sm:hidden before:content-[''] before:absolute before:bg-secondary before:w-full before:h-[1px] before:bottom-0 before:left-0 before:origin-left before:scale-x-0 before:transition-transform h hover:before:scale-x-100 ">Shop</span>
     {/* Hamburger menu */}
